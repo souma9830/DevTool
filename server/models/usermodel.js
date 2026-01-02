8569224f9bdec935869a94fffda8948871a63823
souma9830 <prabirkumarshee8@gmail.com>
2026-01-02 22:48:17 +0530

    UserSchema Created succesfully

---
+++ server/models/usermodel.js
@@ -0,0 +1,15 @@
+import { verify } from "jsonwebtoken";
+import mongoose from "mongoose";
+const userschema=new mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true, unique:true},
+    password:{type:String,required:true},
+    verifyOtp:{type:String,default:''},
+    verifyOtpExpairy:{type:Number,default:0},
+    isverified:{type:Boolean,default:false},
+    resetOtp:{type:String,default:''},
+    resetOtpExpairy:{type:Number,default:0},
+})
+
+const usermodel=mongoose.models.user||mongoose.model('user',userschema);
+export default usermodel;
